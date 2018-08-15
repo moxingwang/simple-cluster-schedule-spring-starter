544b750f4bcf5e607fd3a2a0e573a664aa14ec65
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-08-15 14:53:16 +0800

    commit

--- simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/LotteryApplication.java
+++ simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/LotteryApplication.java
@@ -11,7 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableSimpleScheduleCluster
-@Import({RedisConfiguration.class,ScheduledExecutorServiceConfiguration.class})
+@Import({RedisConfiguration.class, ScheduledExecutorServiceConfiguration.class})
 public class LotteryApplication {
 
     public static void main(String[] args) {
