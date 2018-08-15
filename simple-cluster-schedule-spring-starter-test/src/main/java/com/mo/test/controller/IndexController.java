e21631d8e09cd691e6295e34f5824ed866dfb1b1
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-08-15 15:09:36 +0800

    commit

--- simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/controller/IndexController.java
+++ simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/controller/IndexController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -25,12 +26,13 @@ public class IndexController {
      */
     @GetMapping("/push")
     public String push() {
-        Task task = new Task();
-
+        List<Task> tasks = new ArrayList<>();
         for (int i = 0; i < 200; i++) {
+            Task task = new Task();
             task.setTaskId(UUID.randomUUID().toString());
-            simpleScheduleClusterPublisher.publishTask(Arrays.asList(task));
+            tasks.add(task);
         }
+        simpleScheduleClusterPublisher.publishTask(tasks);
         return "OK";
     }
 }
