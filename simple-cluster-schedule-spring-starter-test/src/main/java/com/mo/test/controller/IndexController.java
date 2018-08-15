cdec508fa2d575c0887b76fb530e5b99bdded083
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-08-15 14:37:43 +0800

    commit

--- simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/controller/IndexController.java
+++ simple-cluster-schedule-spring-starter-test/src/main/java/com/mo/test/controller/IndexController.java
@@ -3,7 +3,6 @@ package com.mo.test.controller;
 import com.mo.schedule.SimpleScheduleClusterPublisher;
 import com.mo.schedule.entity.Task;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,28 +19,14 @@ import java.util.UUID;
 public class IndexController {
     @Autowired
     private SimpleScheduleClusterPublisher simpleScheduleClusterPublisher;
-    @Autowired
-    private RedisTemplate redisTemplate;
 
-    @GetMapping("/test")
-    public String test() throws ClassNotFoundException {
+    /**
+     * 发布任务
+     */
+    @GetMapping("/push")
+    public String push() {
         Task task = new Task();
 
-
-/*        redisTemplate.opsForSet().add(RedisKey.REGISTRY_MACHINE_LIST+1, "1111111");
-        redisTemplate.opsForSet().add(RedisKey.REGISTRY_MACHINE_LIST+1, "3333333333");
-        Set<String> machines = redisTemplate.opsForSet().members(RedisKey.REGISTRY_MACHINE_LIST+1);
-
-
-        redisTemplate.opsForSet().remove(RedisKey.REGISTRY_MACHINE_LIST+1, "1111111");
-        Set<String> machines1 = redisTemplate.opsForSet().members(RedisKey.REGISTRY_MACHINE_LIST+1);
-
-        redisTemplate.opsForSet().remove(RedisKey.REGISTRY_MACHINE_LIST+1, "3333333333");
-
-        Set<String> machines2 = redisTemplate.opsForSet().members(RedisKey.REGISTRY_MACHINE_LIST+1);*/
-
-
-//        task.setTaskClassName(CommonTask.);
         for (int i = 0; i < 200; i++) {
             task.setTaskId(UUID.randomUUID().toString());
             simpleScheduleClusterPublisher.publishTask(Arrays.asList(task));
