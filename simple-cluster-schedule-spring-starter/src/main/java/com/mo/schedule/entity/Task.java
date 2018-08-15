c239e1d4bbff6dbe3f77cfc96b506272a50b0cb7
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-08-15 16:14:33 +0800

    commit

--- simple-cluster-schedule-spring-starter/src/main/java/com/mo/schedule/entity/Task.java
+++ simple-cluster-schedule-spring-starter/src/main/java/com/mo/schedule/entity/Task.java
@@ -2,19 +2,19 @@ package com.mo.schedule.entity;
 
 import java.io.Serializable;
 import java.util.Objects;
-import java.util.UUID;
 
 /**
  * @description: 任务
  * @author: MoXingwang 2018-08-11 12:30
  **/
 public class Task implements Serializable {
+    /**
+     * 指定唯一
+     */
     private String taskId;
     private String taskName;
     private String taskClassName;
 
-
-    private String uuid = UUID.randomUUID().toString();
 
     public String getTaskId() {
         return taskId;
@@ -45,12 +45,12 @@ public class Task implements Serializable {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return Objects.equals(uuid, task.uuid);
+        return Objects.equals(taskId, task.taskId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(uuid);
+        return Objects.hash(taskId);
     }
 
 
